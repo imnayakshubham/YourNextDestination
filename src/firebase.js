17c89b7dad8f6577e4fb8828e415516e4550e02b
Nayak Shubham <70691188+imnayakshubham@users.noreply.github.com>
2021-07-06 21:03:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 
 import "firebase/auth";
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAqYV_Ych5B1rIXeG6qbzAsmssGMMc6ISE",
-  authDomain: "suggestmeplaces-4deac.firebaseapp.com",
-  projectId: "suggestmeplaces-4deac",
-  storageBucket: "process.env.STORAGR_BUCKET",
-  messagingSenderId: "suggestmeplaces-4deac.appspot.com",
-  appId: "1:849037283225:web:a4a2b8f54cefc4d3d20d08",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 });
 export const auth = app.auth();
 export default auth;
