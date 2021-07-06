c6edf54c9ae6501feec18933968bf9c02ae1af97
Nayak Shubham <70691188+imnayakshubham@users.noreply.github.com>
2021-07-06 21:25:59 +0530

    Add files via upload

---
+++ src/components/PageNotFound/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../PageNotFound/ErrorPage.css";
+
+function ErrorPage() {
+  return (
+    <div className="errorpage">
+      <div className="errorpage_content">
+        <h1 className="errorpage_heading">Sorry!! 404 Page Not Found</h1>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorPage;
